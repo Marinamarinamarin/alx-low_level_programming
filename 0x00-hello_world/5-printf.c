5ee6fe8ed7cc7b6f644623ebd28195264857e976
Marinamarinamarin <maghmariam@gmail.com>
2023-03-20 15:47:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function 
+ *
+ * retun: 0 when successfull
+ */
+
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
